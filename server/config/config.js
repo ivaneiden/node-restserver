fb8018386fb3ff7f9cfea713f95fe93b3ed8cc8e
Iván Serrano <iserrano@avla.com>
2020-12-12 22:02:52 -0300

    ADD: se agegar variable heroku

--- server/config/config.js
+++ server/config/config.js
@@ -17,15 +17,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB
 
-// if( process.env.NODE_ENV === 'dev' ){
+if( process.env.NODE_ENV === 'dev' ){
 
-//     urlDB = 'mongodb://localhost:27017/cafe';
+    urlDB = 'mongodb://localhost:27017/cafe';
 
-// } else {
+} else {
     
-    urlDB = 'mongodb+srv://iserrano:umRbvScH58xADfD@cluster0.od4xm.mongodb.net/cafe?retryWrites=true&w=majority';
-
-//}
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
 
